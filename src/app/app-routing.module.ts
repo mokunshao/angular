2a7829e4233b80c2cbb99f6587686ba72de88170
mokunshao <mokunshao@foxmail.com>
2019-12-03 20:10:30 +0800

    路由（Routes）

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PostComponent } from './modules/post/post.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'post', component: PostComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
